c11bcd9471a6511a2e6e8d26ea5884d4958425ee
Harshkamoriya <kamoriyaharsh@gmail.com>
2025-04-18 00:25:52 +0530

    Trigger redeploy with Vercel env vars

--- app/auth/signin/page.jsx
+++ app/auth/signin/page.jsx
@@ -45,7 +45,6 @@ export default function SignIn() {
 
   return (
     <>  
-    <Navbar/>
       <div className="flex min-h-screen items-center justify-center bg-gray-100 p-6">
       <div className="bg-white p-6 rounded-xl shadow-md w-full max-w-md">
         <h2 className="text-2xl font-semibold text-center mb-4">Login to Your Account</h2>
